fc8db6000a98f9b9ee81c2935fad20860501aeb9
Charles-Axel Dein <ca@d3in.org>
2013-05-01 17:26:50 -0700

    Fix push_docs command

--- fabfile.py
+++ fabfile.py
@@ -38,6 +38,7 @@ def push_docs():
 
     docs()
     with lcd("../charlatan-docs/html"):
+        local("git add .")
         local("git commit -am 'Update documentation'")
         local("git push")
 

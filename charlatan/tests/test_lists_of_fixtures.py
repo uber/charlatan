1b1fa69bcbd03b5c5c7f0cc57f416898b00edca9
Erik Formella <erik@uber.com>
2013-11-05 17:52:11 -0800

    Repair test docstrings
    Charles reallly reallly likes docstrings that start with verbs. Now all
    my test doctrings are beginning with "Assert" yey!

--- charlatan/tests/test_lists_of_fixtures.py
+++ charlatan/tests/test_lists_of_fixtures.py
@@ -11,13 +11,15 @@ class TestListOfFixtures(testing.TestCase):
         self.fm.load('./charlatan/tests/data/lists.yaml')
 
     def test_get_list_by_name(self):
-        """A list of fixtures can be retrieved by name"""
+        """Assert lists of fixtures returns lists"""
 
         fixtures = self.fm.install_fixture('fixture_list')
 
         self.assertIsInstance(fixtures, list)
 
     def test_one_to_many_relationship(self):
+        """Assert relations to lists of fixtures work"""
+
         fixture = self.fm.install_fixture('related_fixture')
 
         self.assertEqual(
@@ -26,6 +28,6 @@ class TestListOfFixtures(testing.TestCase):
         )
 
     def test_lists_still_work(self):
-        """Gut check to make sure normal lists can still be made"""
+        """Assert that installing a list as a fixture does not error"""
 
         self.fm.install_fixture('list_fixture')
